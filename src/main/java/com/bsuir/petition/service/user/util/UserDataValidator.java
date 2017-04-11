4eb59952c3c507a0126b18e6871c95d03ff0908d
yankovskiid <yankovskiidv@gmail.com>
2017-04-11 15:29:35 +0300

    Added user data validation

--- src/main/java/com/bsuir/petition/service/user/util/UserDataValidator.java
+++ src/main/java/com/bsuir/petition/service/user/util/UserDataValidator.java
@@ -1,5 +1,13 @@
 package com.bsuir.petition.service.user.util;
 
-public interface UserDataValidator {
+import com.bsuir.petition.bean.dto.user.UpdateUserDTO;
+import com.bsuir.petition.bean.dto.user.UserInformationDTO;
+import com.bsuir.petition.bean.dto.user.UserRegistrationDTO;
+import com.bsuir.petition.service.exception.ErrorInputException;
 
+public interface UserDataValidator {
+    void validate(UserRegistrationDTO userRegistrationDTO) throws ErrorInputException;
+    void validate(UpdateUserDTO updateUserDTO) throws ErrorInputException;
+    void validate(UserInformationDTO userInformationDTO) throws ErrorInputException;
+    void validate(String email) throws ErrorInputException;
 }
--- src/main/java/com/bsuir/petition/service/user/util/impl/UserDataValidatorImpl.java
+++ src/main/java/com/bsuir/petition/service/user/util/impl/UserDataValidatorImpl.java
@@ -1,8 +1,53 @@
 package com.bsuir.petition.service.user.util.impl;
 
+import com.bsuir.petition.bean.dto.user.UpdateUserDTO;
+import com.bsuir.petition.bean.dto.user.UserInformationDTO;
+import com.bsuir.petition.bean.dto.user.UserRegistrationDTO;
+import com.bsuir.petition.service.exception.ErrorInputException;
 import com.bsuir.petition.service.user.util.UserDataValidator;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+
 @Component
 public class UserDataValidatorImpl implements UserDataValidator {
+
+    @Override
+    public void validate(String email) throws ErrorInputException {
+        validateString(email);
+        // TODO: 4/11/17 Add validate email
+    }
+
+    @Override
+    public void validate(UpdateUserDTO updateUserDTO) throws ErrorInputException {
+        ArrayList<String> roles = updateUserDTO.getRoles();
+        if (roles == null || roles.size() == 0) {
+            throw new ErrorInputException("No roles setted!");
+        }
+
+        for (String role : roles) {
+            validateString(role);
+        }
+    }
+
+    @Override
+    public void validate(UserRegistrationDTO userRegistrationDTO) throws ErrorInputException {
+        validateString(userRegistrationDTO.getNick());
+        validateString(userRegistrationDTO.getRepeatPassword());
+        validateString(userRegistrationDTO.getPassword());
+        validate(userRegistrationDTO.getEmail());
+    }
+
+    @Override
+    public void validate(UserInformationDTO userInformationDTO) throws ErrorInputException {
+        validateString(userInformationDTO.getCity());
+        validateString(userInformationDTO.getUsername());
+        validateString(userInformationDTO.getSurname());
+    }
+
+    private void validateString(String data) throws ErrorInputException {
+        if (data == null || data.isEmpty()) {
+            throw new ErrorInputException("Wrong data input, not all data!");
+        }
+    }
 }
