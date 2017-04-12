c17d3e107052b3f5115fce5eca0d4ec0e6f4c919
DmitryArtyomov <artyomovd@yandex.ru>
2017-04-12 11:52:51 +0300

    Added roles CRUD

--- src/main/java/com/bsuir/petition/dao/RoleDao.java
+++ src/main/java/com/bsuir/petition/dao/RoleDao.java
@@ -4,4 +4,9 @@ import com.bsuir.petition.bean.entity.Role;
 
 public interface RoleDao {
     Role getRoleByName(String name);
+    Role getRoleById(Integer id);
+
+    Role createRole(Role role);
+    Role updateRole(Role role);
+    void deleteRole(Role role);
 }
--- src/main/java/com/bsuir/petition/dao/impl/RoleDaoImpl.java
+++ src/main/java/com/bsuir/petition/dao/impl/RoleDaoImpl.java
@@ -29,4 +29,32 @@ public class RoleDaoImpl implements RoleDao {
         role = (Role) query.getSingleResult();
         return role;
     }
+
+    public Role getRoleById(Integer id) {
+        Session session = sessionFactory.getCurrentSession();
+        Query query = session.createQuery("from Role where id = :roleId").setParameter("roleId", id);
+        Role role;
+        role = (Role) query.getSingleResult();
+
+        return role;
+    }
+
+    public Role createRole(Role role) {
+        Session session = sessionFactory.getCurrentSession();
+        session.save(role);
+
+        return role;
+    }
+
+    public Role updateRole(Role role) {
+        Session session = sessionFactory.getCurrentSession();
+        session.saveOrUpdate(role);
+
+        return role;
+    }
+
+    public void deleteRole(Role role) {
+        Session session = sessionFactory.getCurrentSession();
+        session.delete(role);
+    }
 }
