bf99d5a9e0e8aed1ad7966450b06ffbb5d6f8bc3
DmitryArtyomov <artyomovd@yandex.ru>
2017-04-26 16:08:21 +0300

    Updated VOTE CRUD

--- src/main/java/com/bsuir/petition/bean/entity/User.java
+++ src/main/java/com/bsuir/petition/bean/entity/User.java
@@ -32,7 +32,7 @@ public class User extends BaseTable {
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
     private Set<Comment> comments = new HashSet<Comment>();
 
-    @ManyToMany(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"),
                                     inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<Role>();
