0ea43c5b6724c7f5c2113f476c37abf3694c0c25
alex-chuyko <alex.chuyko9@gmail.com>
2017-05-09 21:51:29 +0300

    added buttons in admin documents

--- src/main/resources/app/components/admin/documents/admin.documents.component.ts
+++ src/main/resources/app/components/admin/documents/admin.documents.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpService} from "../../../services/httpServices/http.service";
+import {ShortPetition} from "../../../model/ShortPetition";
 
 @Component({
     templateUrl: 'app/components/admin/documents/admin.documents.component.html',
@@ -7,7 +8,18 @@ import {HttpService} from "../../../services/httpServices/http.service";
 })
 export class AdminDocumentsComponent implements OnInit {
 
+    private petitionsId: number[] = [];
+
     ngOnInit(): void {
+        this.http
+            .getData("/petitions")
+            .subscribe(data => {
+
+                var temp = data.petitions;
+                for (var i = 0; i < temp.length; i++) {
+                    this.petitionsId.push(ShortPetition.deserialize(temp[i]).id);
+                }
+            });
     }
 
     constructor(private http: HttpService) {
